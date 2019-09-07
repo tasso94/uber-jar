e434c424b1765d02a91bac73b8c554d377930f01
Tassilo Weidner <tassilo.weidner@camunda.com>
2019-09-07 10:11:58 +0200

    Ahoy, Shipmates!

---
+++ project/src/main/java/com/camundacon/workshop/MyBeans.java
@@ -0,0 +1,15 @@
+package com.camundacon.workshop;
+
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBeans {
+
+  @Bean
+  public JavaDelegate ahoyService() {
+    return execution -> System.out.println("\n\nAhoy, " + execution.getVariable("myVariable") + "!");
+  }
+
+}
